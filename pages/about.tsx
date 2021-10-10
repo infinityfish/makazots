4f0f2d70f23154ede9ec75c48bb13c2b2c3ade0b
ericmabo <ericmabo@gmail.com>
2021-10-10 01:59:15 -0400

    installed MUI v5

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
-import Link from 'next/link';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Link from 'next/link';
 
-export default function Index() {
+export default function About() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Makazo with TypeScript
+          About Us
         </Typography>
-        <Link href="/about">About Us</Link>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
       </Box>
     </Container>
   );
